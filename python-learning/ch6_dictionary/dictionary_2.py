ca680fc1762a5cb8789de4b7a188f6db0d593ec8
Daniil-redkod <redkod02@gmail.com>
2026-07-20 17:21:05 +0300

    День 13 обучения 146 стр.

--- python-learning/ch6_dictionary/dictionary_2.py
+++ python-learning/ch6_dictionary/dictionary_2.py
@@ -34,3 +34,60 @@ for alien in aliens[:5]:
     print(alien)
 
 # Список в словаре
+# pizza.py
+# Сохранение информации о заказанной пиццы
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+    }
+
+# Описание заказа
+print(f"You ordered a {pizza['crust']} - crust pizza "
+      "with the following toppings")
+
+for toping in pizza['toppings']:
+    print(f"\t{toping}")
+
+# Пример с языками
+
+favorite_languages = {
+                'jen': ['python', 'c'],
+                'sarah': ['c'],
+                'edward': ['rust', 'c#'],
+                'phil': ['python', 'java'],
+                }
+
+friends = ['bob', 'anton', 'jen', 'sarah', 'edward', 'phil']
+
+
+for name in friends:
+    if name in favorite_languages:
+        if len(favorite_languages[name]) == 1:
+            print(f"{name} - любит этот язык программирования ")
+        else:
+            print(f"{name} - любит эти языки программирования ")
+        for language in favorite_languages[name]:
+            print(f"\t{language}")
+    else:
+        print(f"{name} - необходимо пройти опрос")
+
+# Вложение словарей в словарь
+# many_users.py
+users = {
+    'maximus': {
+        'first': 'max',
+        'last': 'popov',
+        'location': 'moscow',
+        },
+
+    'sis01': {
+        'first': 'sis',
+        'last': 'zola',
+        'location': 'paris',
+        },
+}
+
+for user_name, user_info in users.items():
+    print(user_name)
+    for first, name in user_info.items():
+        print(f"\t{first} - {name}")
